32c1ecce5e0efb8c1990b552109112053422eb4c
vijay Kumar <surya991837@gmail.com>
2025-06-25 02:07:05 +0530

    seed user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
-import cookieParsar from "cookie-parser";
+import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.middleware.js";
+
+import { createUser } from "./seeders/user.seeder.js";
+
 const app = express();
 
 // CORS configuration
@@ -15,10 +18,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParsar());
-
-//  router import
-import userRouter from "./routes/user.route.js";
+app.use(cookieParser());
 
 // index router
 app.get("/", (req, res) => {
@@ -26,35 +26,43 @@ app.get("/", (req, res) => {
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Surya's Page</title>
-        <style>
-          body {
-            background-color: #0f172a;
-            color: #facc15;
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-          }
-          h1 {
-            font-size: 3rem;
-          }
-        </style>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Surya's Page</title>
+    <style>
+    body {
+      background-color: #0f172a;
+      color: #facc15;
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+    h1 {
+      font-size: 3rem;
+    }
+    </style>
     </head>
     <body>
-        <h1>This is Index  Page and Server is Running  </h1>
+        <h1>This is Index Page and Server is Running</h1>
     </body>
     </html>
   `);
 });
 
-// api-end point
-app.use("/api/v1/users", userRouter); // api-url - http://localhost:8000/api/v1/users
-//  router middleware
+// createUser(10)
 
+// Routers
+import userRouter from "./routes/user.route.js";
+import chatRouter from "./routes/chat.route.js";
+
+// API Endpoints
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/chats", chatRouter);
+
+// Error handling middleware
 app.use(errorMiddleware);
+
 export default app;
